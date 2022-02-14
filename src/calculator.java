d252baff29365ecc51fbeec5ff3e05e7d19b1747
samuelran <samuelbesserransby@gmail.com>
2022-02-14 22:57:43 +0100

    Initial commit

---
+++ src/calculator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class calculator {
+    public static void binaryToDecimalConverter() {
+        Scanner input = new Scanner(System.in);
+        //en scanner implementeres for at tage imod brugerens input
+        System.out.print("Type a number in binary that you want converted ");
+        //når programmet køres printes der hvilket tal programmet tager imod
+        String binaryString = input.nextLine();
+        //Her laves der en String ud fra det input der er givet af brugeren
+        System.out.println("Number converted from binary " + Integer.parseInt(binaryString, 2));
+        //Her hentes parseInt metoden fra Integer-klassen, der gør at binaryString laves om til en integer og printes.
+    }
+}
