6d4163ca25b4cb4dbc3881bb964654d99cca45e0
samuelran <samuelbesserransby@gmail.com>
2022-02-14 22:59:27 +0100

    changes

--- src/calculator.java
+++ src/calculator.java
@@ -6,9 +6,9 @@ public class calculator {
         //en scanner implementeres for at tage imod brugerens input
         System.out.print("Type a number in binary that you want converted ");
         //når programmet køres printes der hvilket tal programmet tager imod
-        String binaryString = input.nextLine();
+        String stringOfBinaryNumbers = input.nextLine();
         //Her laves der en String ud fra det input der er givet af brugeren
-        System.out.println("Number converted from binary " + Integer.parseInt(binaryString, 2));
+        System.out.println("Number converted from binary " + Integer.parseInt(stringOfBinaryNumbers, 2));
         //Her hentes parseInt metoden fra Integer-klassen, der gør at binaryString laves om til en integer og printes.
     }
 }
